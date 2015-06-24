892124fd8932660602d35151e25f89efd570ba68
Alex Stroulger <alexstroulger@gmail.com>
2015-06-24 10:59:30 -0400

    Initial Commit

---
+++ ToDoList/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  ToDoList
+//
+//  Created by Alex on 2015-06-24.
+//  Copyright (c) 2015 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
